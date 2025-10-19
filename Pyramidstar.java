39eca58c18eb3fe6a332d76b3167a7d3174e257a
surajbharsakle07 <bharsaklesuraj44@gmail.com>
2025-10-19 22:18:46 +0530

    Create Pyramidstar.java

---
+++ Pyramidstar.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+
+public class Pyramidstar {
+    
+    public static void PyramidStar(int n)
+    {
+        int a, b;
+
+        for (a = 0; a < n; a++) {
+
+            for (b = 0; b <= a; b++) {
+              
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int k = 5;
+        PyramidStar(k);
+    }
+}
